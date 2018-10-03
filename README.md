0f20dab9671906ae6f7a1c1c7d42604d14eae4fb
a-windisch <andreas.windisch@yahoo.com>
2018-10-03 16:59:22 -0500

    data added

---
+++ README.md
@@ -0,0 +1,14 @@
+## Flight Trajectory Analysis   
+[Andreas Windisch](https://www.linkedin.com/in/andreas-windisch-physics/)
+   
+### 0 Synopsis   
+In this repository, I collect and analyze some of the data that I recorded from the sensors of my cell phone when plioting general aviation aircraft. Apart from position (WGS84), I also recorded altitude, magnetic heading, ground speed, and data from the accelerometers.
+Feel free to use the data as you please. You can also find and connect with me on LinkedIn.
+
+### 1 Data Files in this repository   
+
+-- C152_N53398_KCPS_to_KSLO_2017-10-29.csv     
+-- DA20-C1_N107TX_KSUS_to_1H3_2018-07-17.csv   
+-- DA20-C1_N107TX_KSUS_to_KMYJ_2018-09-15.csv   
+
+      
---
+++ trajectories.R
@@ -0,0 +1,42 @@
+setwd("/home/andreas/flight_trajectories/R_scripts/")
+
+#load flight data and get relevant columns
+flt1<-read.csv("C152_N53398_KCPS_to_KSLO_2017-10-29.csv")
+flt2<-read.csv("DA20-C1_N107TX_KSUS_to_1H3_2018-07-17.csv")
+flt3<-read.csv("DA20-C1_N107TX_KSUS_to_KMYJ_2018-09-15.csv")
+dat1<-flt1[,c(6,7,8,9,19)]
+dat2<-flt2[,c(6,7,8,9,19)]
+dat3<-flt3[,c(6,7,8,9,19)]
+names(dat1) <- c("lat","lon","alt","spd","hdg")
+names(dat2) <- c("lat","lon","alt","spd","hdg")
+names(dat3) <- c("lat","lon","alt","spd","hdg")
+
+#landing 1 start and end indices (empirically) 
+ls1 <- 2580
+le1 <- 2622
+ldg1data <- dat1[ls1:le1,]
+
+#load leaflet
+library(leaflet)
+
+#create plots for flt1
+fullmap1 <- leaflet(dat1) %>% 
+    addTiles() %>%
+    addCircles(~lon, ~lat, weight = 3, radius=40, 
+               color="#ff0000", stroke = TRUE, fillOpacity = 0.8)
+ 
+ ldgmap1 <- leaflet(ldg1data) %>% 
+            addTiles() %>%
+            addCircles(~lon, ~lat, weight = 3, radius=40, color="#ff0000", stroke = TRUE, fillOpacity = 0.8)
+ 
+ #create plots for flt2
+ fullmap2 <- leaflet(dat2) %>% 
+    addTiles() %>%
+    addCircles(~lon, ~lat, weight = 3, radius=40, 
+               color="#ff0000", stroke = TRUE, fillOpacity = 0.8)
+ 
+ #create plots for flt3
+ fullmap3 <- leaflet(dat3) %>% 
+    addTiles() %>%
+    addCircles(~lon, ~lat, weight = 3, radius=40, 
+               color="#ff0000", stroke = TRUE, fillOpacity = 0.8)
